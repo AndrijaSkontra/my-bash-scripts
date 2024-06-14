56a38cc4137c56905e3d35fd4529fbba5839950d
AndrijaSkontra <askontra22@student.unizd.hr>
2024-06-14 15:16:54 +0200

    change startservers to use windows instead of panes

--- startservers.sh
+++ startservers.sh
@@ -3,21 +3,22 @@
 # Create a new tmux session named 'RWA-Project'
 tmux new-session -d -s RWA-Project
 
-# Rename the window to 'Development'
-tmux rename-window -t RWA-Project 'Development'
+# Rename the first window to 'Frontend'
+tmux rename-window -t RWA-Project:0 'Frontend'
 
-# Split the window into two panes
-tmux split-window -h
+# In the 'Frontend' window, navigate to the frontend directory and run 'ng serve --watch'
+tmux send-keys -t RWA-Project:0 'cd /home/shkoki/school/RWA-Project/frontend' C-m
+tmux send-keys -t RWA-Project:0 'ng serve --watch' C-m
 
-# In the first pane, navigate to the frontend directory and run 'ng serve --watch'
-tmux send-keys -t RWA-Project:0.0 'cd /home/shkoki/school/RWA-Project/frontend' C-m
-tmux send-keys -t RWA-Project:0.0 'ng serve --watch' C-m
+# Create a new window named 'Backend'
+tmux new-window -t RWA-Project -n 'Backend'
 
-# In the second pane, navigate to the backend directory and run 'nest start --watch'
-tmux send-keys -t RWA-Project:0.1 'cd /home/shkoki/school/RWA-Project/backend' C-m
-tmux send-keys -t RWA-Project:0.1 'nest start --watch' C-m
+# In the 'Backend' window, navigate to the backend directory and run 'nest start --watch'
+tmux send-keys -t RWA-Project:1 'cd /home/shkoki/school/RWA-Project/backend' C-m
+tmux send-keys -t RWA-Project:1 'nest start --watch' C-m
 
 # Attach to the session
 tmux attach-session -t RWA-Project
 
 echo -e "\033]0;serveri\007"
+
